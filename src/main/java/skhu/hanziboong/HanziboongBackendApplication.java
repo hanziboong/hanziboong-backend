17f2c205774d37bf7cac3ab62c79259c90b0aa5a
hangillee <coderoad.io@gmail.com>
2025-04-06 17:19:37 +0900

    first commit

---
+++ src/main/java/skhu/hanziboong/HanziboongBackendApplication.java
@@ -0,0 +1,13 @@
+package skhu.hanziboong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanziboongBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanziboongBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skhu/hanziboong/HanziboongBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package skhu.hanziboong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanziboongBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

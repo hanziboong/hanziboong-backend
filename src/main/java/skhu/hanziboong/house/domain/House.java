f9bfd392039d3f8c3908a77cbe0435ff6fd0d248
woogym <kwj9294@gmail.com>
2025-04-14 23:47:24 +0900

    feat: schedule 연관관계 매핑 추가

--- src/main/java/skhu/hanziboong/house/domain/House.java
+++ src/main/java/skhu/hanziboong/house/domain/House.java
@@ -1,16 +1,22 @@
 package skhu.hanziboong.house.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import skhu.hanziboong.global.BaseEntity;
+import skhu.hanziboong.schedule.domain.Schedule;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -24,6 +30,10 @@ public class House extends BaseEntity {
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private HouseType houseType;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
 
     public House(HouseType houseType) {
         this.houseType = houseType;

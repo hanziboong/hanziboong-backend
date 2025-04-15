e9b66b6f12c7240a5efb66f7c06966fb03dc7dba
woogym <kwj9294@gmail.com>
2025-04-15 16:46:46 +0900

    feat: Schedule 생성 api 구현

---
+++ src/main/java/skhu/hanziboong/schedule/controller/ScheduleController.java
@@ -0,0 +1,25 @@
+package skhu.hanziboong.schedule.controller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import skhu.hanziboong.schedule.dto.request.ScheduleRequest;
+import skhu.hanziboong.schedule.service.ScheduleService;
+
+@RestController
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@RequestMapping("/api/schedule")
+public class ScheduleController {
+
+    private final ScheduleService scheduleService;
+
+    @PostMapping
+    public ResponseEntity<Void> createSchedule(@RequestBody ScheduleRequest request) {
+        scheduleService.createSchedule(request);
+        return ResponseEntity.ok().build();
+    }
+}

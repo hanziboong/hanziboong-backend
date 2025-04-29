8802957fa5a8a2e61e293c47b54cddb40fb5e38b
hangillee <skfcb10@naver.com>
2025-04-30 02:54:26 +0900

    feat: 집, 연도, 월 기준 일정 조회 쿼리 메소드 작성

--- src/main/java/skhu/hanziboong/schedule/repository/ScheduleRepository.java
+++ src/main/java/skhu/hanziboong/schedule/repository/ScheduleRepository.java
@@ -1,7 +1,14 @@
 package skhu.hanziboong.schedule.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import skhu.hanziboong.house.domain.House;
 import skhu.hanziboong.schedule.domain.Schedule;
 
+@Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    List<Schedule> findByHouseAndStartAtBetween(House house, LocalDateTime start, LocalDateTime end);
 }

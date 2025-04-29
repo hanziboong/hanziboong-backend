b122f5920cae778b59df890116d9952640a2c3c4
hangillee <skfcb10@naver.com>
2025-04-30 02:42:02 +0900

    feat: 일정 참가자 조회 쿼리 메소드 추가

--- src/main/java/skhu/hanziboong/schedule/repository/ScheduleParticipantRepository.java
+++ src/main/java/skhu/hanziboong/schedule/repository/ScheduleParticipantRepository.java
@@ -1,7 +1,13 @@
 package skhu.hanziboong.schedule.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import skhu.hanziboong.schedule.domain.Schedule;
 import skhu.hanziboong.schedule.domain.ScheduleParticipant;
 
+@Repository
 public interface ScheduleParticipantRepository extends JpaRepository<ScheduleParticipant, Long> {
+    
+    List<ScheduleParticipant> findAllBySchedule(Schedule schedule);
 }

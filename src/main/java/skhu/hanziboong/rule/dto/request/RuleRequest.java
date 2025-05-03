69e77ecd849d8fc8f2594ae2d332b74b950bb50b
hangillee <skfcb10@naver.com>
2025-05-04 05:28:19 +0900

    refactor: House, Member 도메인 구현으로 Rule 생성용 DTO 리팩터링

--- src/main/java/skhu/hanziboong/rule/dto/request/RuleRequest.java
+++ src/main/java/skhu/hanziboong/rule/dto/request/RuleRequest.java
@@ -1,18 +1,14 @@
 package skhu.hanziboong.rule.dto.request;
 
-import skhu.hanziboong.house.domain.House;
-import skhu.hanziboong.house.domain.HouseType;
 import skhu.hanziboong.member.domain.Member;
 import skhu.hanziboong.rule.domain.Rule;
 
 public record RuleRequest(
         String title,
-        String description
+        String description,
+        Long memberId
 ) {
-    public Rule toRule() {
-        // TODO: house와 member는 임시 객체이므로 기능 구현 후 대체해야 함.
-        House house = new House(HouseType.MONTHLY_RENT);
-        Member member = new Member("username", "nickname", house);
+    public Rule toRule(Member member) {
         return new Rule(title, description, member);
     }
 }

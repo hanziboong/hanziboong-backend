761b944abf0c87745184dccfcc4740cbc69b6fa2
woogym <kwj9294@gmail.com>
2025-06-06 15:10:23 +0900

    feat: 공동 지출 내역 entity 설계

---
+++ src/main/java/skhu/hanziboong/ledger/domain/Expense.java
@@ -0,0 +1,58 @@
+package skhu.hanziboong.ledger.domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import skhu.hanziboong.global.BaseEntity;
+import skhu.hanziboong.house.domain.House;
+import skhu.hanziboong.member.domain.Member;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Expense extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Long expenditure;
+
+    private String memo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private House house;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member paidBy;
+
+    @OneToMany(mappedBy = "expense", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ExpenseParticipant> participants = new ArrayList<>();
+
+    @Builder
+    public Expense(String title, Long expenditure, String memo, House house, Member paidBy,
+                   List<ExpenseParticipant> participants) {
+        this.title = title;
+        this.expenditure = expenditure;
+        this.memo = memo;
+        this.house = house;
+        this.paidBy = paidBy;
+        this.participants = participants;
+    }
+}

1ce19b0feb7fc416b52f7bcf82173883ef0a4276
woogym <kwj9294@gmail.com>
2025-06-09 01:42:32 +0900

    feat: 공동 지출 참여자 목록 조회 쿼리 작성

--- src/main/java/skhu/hanziboong/member/repository/MemberRepository.java
+++ src/main/java/skhu/hanziboong/member/repository/MemberRepository.java
@@ -1,7 +1,11 @@
 package skhu.hanziboong.member.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import skhu.hanziboong.house.domain.House;
 import skhu.hanziboong.member.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByIdInAndHouseId(List<Long> participantMemberId, Long houseId);
 }

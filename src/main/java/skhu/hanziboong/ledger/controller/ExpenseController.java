b32c523d77f552b1592535443430ab05a41d8c72
woogym <kwj9294@gmail.com>
2025-06-09 05:22:42 +0900

    feat: 공동 지출 내역 전체 조회 api 추가

--- src/main/java/skhu/hanziboong/ledger/controller/ExpenseController.java
+++ src/main/java/skhu/hanziboong/ledger/controller/ExpenseController.java
@@ -1,10 +1,17 @@
 package skhu.hanziboong.ledger.controller;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.net.URI;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +34,19 @@ public class ExpenseController implements ExpenseApiDocs {
         ExpenseResponse response = expenseService.createExpense(request);
 
         return ResponseEntity.created(URI.create("api/expense" + response.id())).build();
+    }
+
+    @Override
+    @GetMapping("/{houseId}")
+    public ResponseEntity<Page<ExpenseResponse>> findExpensesByHouse(
+            @PathVariable
+            Long houseId,
+            @Parameter(hidden = true)
+            @PageableDefault(size = 10, sort = "createdAt", direction = Direction.DESC)
+            Pageable pageable) {
+
+            Page<ExpenseResponse> responses = expenseService.findExpensesByHouseId(houseId, pageable);
+
+        return ResponseEntity.ok(responses);
     }
 }
--- src/main/java/skhu/hanziboong/ledger/controller/docs/ExpenseApiDocs.java
+++ src/main/java/skhu/hanziboong/ledger/controller/docs/ExpenseApiDocs.java
@@ -6,14 +6,17 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import skhu.hanziboong.global.exception.model.BadRequestException;
 import skhu.hanziboong.ledger.dto.request.ExpenseRequest;
+import skhu.hanziboong.ledger.dto.response.ExpenseResponse;
 
 @Tag(name = "가계부", description = "가계부 관리 API")
 public interface ExpenseApiDocs {
 
-    @Operation(summary = "가계부 생성")
+    @Operation(summary = "공동 지출 내역 생성")
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "201",
@@ -26,4 +29,18 @@ public interface ExpenseApiDocs {
             ),
     })
     ResponseEntity<Void> createExpense(ExpenseRequest request);
+
+    @Operation(summary = "공동 지출 내역 전체 조회")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "요청이 정상적으로 처리되었을 때"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "요청 Body에 올바르지 않은 값이 전달되었을 때",
+                    content = @Content(schema = @Schema(implementation = BadRequestException.class))
+            ),
+    })
+    ResponseEntity<Page<ExpenseResponse>> findExpensesByHouse(Long houseId, Pageable pageable);
 }

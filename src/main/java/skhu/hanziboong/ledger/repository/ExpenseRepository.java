1d65535a2c6a0fa70455234ff68d6c609e6f3787
woogym <kwj9294@gmail.com>
2025-06-09 05:20:33 +0900

    fix: N+1 문제 해결을 위한 fetch join 으로 조회하는 쿼리문으로 수정

--- src/main/java/skhu/hanziboong/ledger/repository/ExpenseRepository.java
+++ src/main/java/skhu/hanziboong/ledger/repository/ExpenseRepository.java
@@ -1,8 +1,20 @@
 package skhu.hanziboong.ledger.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import skhu.hanziboong.house.domain.House;
 import skhu.hanziboong.ledger.domain.Expense;
 
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
+    @Query("""
+    SELECT DISTINCT e FROM Expense e
+    LEFT JOIN FETCH e.participants p
+    LEFT JOIN FETCH p.participantMember
+    WHERE e.house = :house
+    """)
+    List<Expense> findAllByHouseWithParticipants(@Param("house") House house);
 }

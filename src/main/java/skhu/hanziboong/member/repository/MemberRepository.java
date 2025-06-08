c8d91f9b266f117bb2d4c12838f3ef921873c017
woogym <kwj9294@gmail.com>
2025-06-09 03:08:40 +0900

    chore: house에 속한 member Id 조회 쿼리문 수정

--- src/main/java/skhu/hanziboong/member/repository/MemberRepository.java
+++ src/main/java/skhu/hanziboong/member/repository/MemberRepository.java
@@ -2,10 +2,9 @@ package skhu.hanziboong.member.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import skhu.hanziboong.house.domain.House;
 import skhu.hanziboong.member.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    List<Member> findByIdInAndHouseId(List<Long> participantMemberId, Long houseId);
+    List<Member> findByIdInAndHouse_Id(List<Long> participantMemberId, Long houseId);
 }

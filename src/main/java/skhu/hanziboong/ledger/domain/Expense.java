694a2f767397cd743c640132e558b6a19f81d9e0
woogym <kwj9294@gmail.com>
2025-06-10 20:19:51 +0900

    feat: 공동 지출 내역 도메인 유효성 검사 추가

--- src/main/java/skhu/hanziboong/ledger/domain/Expense.java
+++ src/main/java/skhu/hanziboong/ledger/domain/Expense.java
@@ -49,6 +49,9 @@ public class Expense extends BaseEntity {
     @Builder
     public Expense(String title, Long expenditure, String memo,
                    House house, Member paidBy) {
+        validateNotBlank(title);
+        validateExpenditureIsNegative(expenditure);
+        validateNotNull(house, paidBy);
         this.title = title;
         this.expenditure = expenditure;
         this.memo = memo;
@@ -57,6 +60,8 @@ public class Expense extends BaseEntity {
     }
 
     public void update(String title, Long expenditure, String memo) {
+        validateNotBlank(title);
+        validateExpenditureIsNegative(expenditure);
         this.title = title;
         this.expenditure = expenditure;
         this.memo = memo;
@@ -74,5 +79,27 @@ public class Expense extends BaseEntity {
     // 정산시에 소수점에 대한 부분은 아직 고려하지 않았습니다. 부동소수점 연산으로 오차 없이 할 수 있을 것 같은데 추후에 회의해보고 도입해보겠습니다.
     private Long calculateSettleAmount(List<Member> expenseParticipants) {
         return this.expenditure / expenseParticipants.size();
+    }
+
+    private void validateNotBlank(String title) {
+        if (title == null || title.isBlank()) {
+            throw new IllegalArgumentException("공동 지출 내역의 제목은 비어있을 수 없습니다.");
+        }
+    }
+
+    private void validateExpenditureIsNegative(Long expenditure) {
+        if (expenditure < 0) {
+            throw new IllegalArgumentException("공동 지출 금액이 음수 일 수 없습니다.");
+        }
+    }
+
+    private void validateNotNull(House house, Member paidBy) {
+        if (house == null) {
+            throw new IllegalArgumentException("주거지 정보는 null일 수 없습니다.");
+        }
+
+        if (paidBy == null) {
+            throw new IllegalArgumentException("공동지출내역 지불자는 null일 수 없습니다.");
+        }
     }
 }

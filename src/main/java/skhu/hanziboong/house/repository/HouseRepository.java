2b30ed6080eb31a586f72f99f9558980b11d3317
woogym <kwj9294@gmail.com>
2025-06-17 14:56:44 +0900

    feat: 사야 할 물건 도메인 조회by집 쿼리 추가

--- src/main/java/skhu/hanziboong/house/repository/HouseRepository.java
+++ src/main/java/skhu/hanziboong/house/repository/HouseRepository.java
@@ -1,7 +1,13 @@
 package skhu.hanziboong.house.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import skhu.hanziboong.house.domain.House;
 
 public interface HouseRepository extends JpaRepository<House, Long> {
+
+    @Query("select h from House h left join fetch h.toBuyItems where h.id = :id")
+    Optional<House> findWithToBuyItemsById(@Param("id") Long id);
 }

82302feaca4d82928a061f8423dad4d19f027bd3
woogym <kwj9294@gmail.com>
2025-06-20 18:48:50 +0900

    refactor: 사용하지 않는 쿼리 삭제

--- src/main/java/skhu/hanziboong/house/repository/HouseRepository.java
+++ src/main/java/skhu/hanziboong/house/repository/HouseRepository.java
@@ -8,6 +8,5 @@ import skhu.hanziboong.house.domain.House;
 
 public interface HouseRepository extends JpaRepository<House, Long> {
 
-    @Query("select h from House h left join fetch h.toBuyItems where h.id = :id")
-    Optional<House> findWithToBuyItemsById(@Param("id") Long id);
+
 }

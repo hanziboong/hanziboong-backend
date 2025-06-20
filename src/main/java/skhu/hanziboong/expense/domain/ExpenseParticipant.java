dddc413c8d9f04d221ad2537bcf4f3b2b223aa64
woogym <kwj9294@gmail.com>
2025-06-20 18:19:50 +0900

    feat: 주석 제거

--- src/main/java/skhu/hanziboong/expense/domain/ExpenseParticipant.java
+++ src/main/java/skhu/hanziboong/expense/domain/ExpenseParticipant.java
@@ -35,14 +35,6 @@ public class ExpenseParticipant extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     private Member participantMember;
 
-    /** 기존에는 Expense -> ExpenseParticipant 단방향 구조였지만 조회, 수정, 삭제, 정렬, 통계 집계등
-     다양한 쿼리 대상이 될 것이여서 양방향으로 설계했습니다.
-     하지만 현재 참여한 인원의 정산 상태를 위한 테이블이 Expense를 알아야할까?에 대한 의문이 들었습니다.
-     외래키 관리, 영속성 전이의 일관성에 대한 주의, insert보다 update쿼리를 더 많이 발생 시킴 (컬렉션 교체시에 전체 delete후에 insert로 동작)
-     이러한 이유들로 인해서 도메인 요구사항에 대한 열림(?) vs 유지보수와 성능 이 2가지로 고민했습니다
-     결국 유지보수와 성능을 선택해서 양방향으로 설계를 수정했습니다.
-     이 부분 어떻게 생각하시는지 궁금합니다ㅠ
-     **/
     @ManyToOne(fetch = FetchType.LAZY)
     private Expense expense;
 

045bb70517b3c01b32f78dd9f8025e50532bdbfd
hangillee <skfcb10@naver.com>
2025-06-23 03:32:49 +0900

    feat: 집 ID 기준 집에 소속된 모든 사용자 조회 API 구현

--- src/main/java/skhu/hanziboong/house/controller/HouseController.java
+++ src/main/java/skhu/hanziboong/house/controller/HouseController.java
@@ -7,16 +7,19 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.net.URI;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import skhu.hanziboong.global.exception.model.BadRequestException;
 import skhu.hanziboong.house.dto.request.HouseRequest;
 import skhu.hanziboong.house.dto.response.HouseCreateResponse;
 import skhu.hanziboong.house.service.HouseService;
+import skhu.hanziboong.member.dto.response.MemberResponse;
 
 @Tag(name = "집")
 @RequiredArgsConstructor
@@ -43,5 +46,24 @@ public class HouseController {
         HouseCreateResponse response = houseService.createHouse(request);
 
         return ResponseEntity.created(URI.create("/api/houses/" + response.id())).build();
+    }
+
+    @Operation(summary = "집에 소속된 모든 사용자 조회")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "요청이 정상적으로 처리되었을 때"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "요청 Body에 올바르지 않은 값이 전달되었을 때",
+                    content = @Content(schema = @Schema(implementation = BadRequestException.class))
+            ),
+    })
+    @GetMapping("/house/{id}/members")
+    public ResponseEntity<List<MemberResponse>> getMembers(@PathVariable Long id) {
+        List<MemberResponse> responses = houseService.findMembersById(id);
+
+        return ResponseEntity.ok(responses);
     }
 }
--- src/main/java/skhu/hanziboong/house/service/HouseService.java
+++ src/main/java/skhu/hanziboong/house/service/HouseService.java
@@ -1,5 +1,6 @@
 package skhu.hanziboong.house.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,17 +8,31 @@ import skhu.hanziboong.house.domain.House;
 import skhu.hanziboong.house.dto.request.HouseRequest;
 import skhu.hanziboong.house.dto.response.HouseCreateResponse;
 import skhu.hanziboong.house.repository.HouseRepository;
+import skhu.hanziboong.member.dto.response.MemberResponse;
+import skhu.hanziboong.member.repository.MemberRepository;
 
 @RequiredArgsConstructor
 @Service
 public class HouseService {
 
     private final HouseRepository houseRepository;
+    private final MemberRepository memberRepository;
 
     @Transactional
     public HouseCreateResponse createHouse(HouseRequest request) {
         House house = houseRepository.save(request.toHouse());
 
         return HouseCreateResponse.from(house);
+    }
+
+    @Transactional(readOnly = true)
+    public List<MemberResponse> findMembersById(Long id) {
+        House house = houseRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 집입니다."));
+
+        return memberRepository.findAllByHouse(house)
+                .stream()
+                .map(MemberResponse::from)
+                .toList();
     }
 }
